c0cc2bf93466512df6fd03cdc5cbc66cecebed9d
Lord-Einstein <darylleinstein@gmail.com>
2025-12-10 13:47:25 +0100

    Feat : Add calories_lign function to detect a lign who contains a calories value.

--- day-04/parser.c
+++ day-04/parser.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CONVERSION_BASE 10
+
 int empty_lign(char *lign) {
     char matches_char;
     if(sscanf(lign, " %c", &matches_char) != 1) {
         printf("-EMPTY LIGN-\n");
         return 1;
     }
+    return 0;
+}
+
+unsigned long calories_lign(char* lign) {
+    unsigned long calories_value;
+    calories_value = (lign, NULL, CONVERSION_BASE);
+
+    if(calories_value) {
+        return calories_value;
+    }
+
+    //de toutes façons si la valeur convertie est vraiment de 0 on n'en a rien à faire :p .. cà n'influe pas sur le calcul
     return 0;
 }
 
@@ -19,6 +33,8 @@ int main(void) {
 
     FILE *file = NULL;
     char lign_buffer[256];
+    ElfeCalories elfe_calories_table[100];
+    int elfe_cursor = 0;
 
 
     file = fopen("./test.txt", "a+");
