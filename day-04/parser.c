689d5c46c04a429ccb5fc224f1a9e9e138b9473d
Lord-Einstein <darylleinstein@gmail.com>
2025-12-10 13:13:31 +0100

    Feat : Create empty-lign who test if one line is empty.

---
+++ day-04/parser.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int empty_lign(char *lign) {
+    char matches_char;
+    if(sscanf(lign, " %c", matches_char) != 1) {
+        return 1;
+    }
+    return 0;
+}
+
+int main(void) {
+
+    FILE *file = NULL;
+    char lign_buffer[256];
+
+
+    file = fopen("./test.txt", "a+");
+
+    if(!file){
+        perror("Erreur de traitement du fichier : (fopen)");
+        return EXIT_FAILURE;
+    }
+
+    while(fgets(lign_buffer, sizeof(lign_buffer), file) != NULL) {
+        if(empty_lign(lign_buffer)) {
+            printf("-EMPTY LIGN-");
+            continue;
+        }
+        printf("%s", lign_buffer);
+    }
+
+
+    fclose(file);
+    return EXIT_SUCCESS;
+}
